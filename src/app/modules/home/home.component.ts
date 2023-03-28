38242048c80d544487e0f62f5940ad57c736a534
thisislamer <mangushev.maks13@gmail.com>
2023-03-28 15:01:24 +0300

    update: home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IGroup } from '@core/services/dto/group.dto';
+import { GroupsService } from '@core/services/groups.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  public groups: IGroup[];
+
+  constructor(private groupsService: GroupsService) {}
+
+  ngOnInit() {
+    this.groupsService.get().subscribe((groups) => {
+      this.groups = groups;
+    });
+  }
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { LayoutModule } from '@core/layout/layout.module';
+import { GroupsService } from '@core/services/groups.service';
 import { HomeComponent } from './home.component';
 
 const routes: Route[] = [
@@ -13,6 +15,7 @@ const routes: Route[] = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), LayoutModule],
+  providers: [GroupsService],
 })
 export class HomeModule {}
