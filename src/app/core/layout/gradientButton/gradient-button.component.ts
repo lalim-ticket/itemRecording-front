de23fc4c5327ee3521501095cde13cdae8bf17c9
thisislamer <mangushev.maks13@gmail.com>
2023-03-29 21:29:21 +0300

    add: gradient-button-component

---
+++ src/app/core/layout/gradientButton/gradient-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gradient-button',
+  templateUrl: './gradient-button.component.html',
+  styleUrls: ['./gradient-button.component.scss'],
+})
+export class GradientButtonComponent implements OnInit {
+  ngOnInit(): void {
+    return;
+  }
+}
