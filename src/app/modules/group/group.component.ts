bb8b2de46769c0805ee94c5df82793c0927e6fa2
thisislamer <mangushev.maks13@gmail.com>
2023-04-01 23:27:41 +0300

    update: item, add: get history

--- src/app/modules/group/group.component.ts
+++ src/app/modules/group/group.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IGroup } from '@core/services/dto/group.dto';
+import { IHistory } from '@core/services/dto/history.dto';
+import { IItem } from '@core/services/dto/item.dto';
 import { GroupsService } from '@core/services/groups.service';
+import { HistoryService } from '@core/services/history.service';
 import { ItemsService } from '@core/services/items.service';
 
 @Component({
@@ -12,19 +15,27 @@ import { ItemsService } from '@core/services/items.service';
 export class GroupComponent implements OnInit {
   public groupId: string;
   public group: IGroup;
+  public selectedItem: IItem;
 
   public username: string;
 
   public showAddUserModal = false;
   public showCreateItemModal = false;
+  public showEditItemModal = false;
+  public showHistoryModal = false;
 
   public itemName: string;
   public itemDescription: string;
+  public itemCount: number;
+
+  public history: IHistory[];
+  public offsetHistory: number;
 
   constructor(
     private readonly activateRoute: ActivatedRoute,
     private readonly groupsService: GroupsService,
     private readonly itemsService: ItemsService,
+    private readonly historyService: HistoryService,
   ) {}
 
   ngOnInit() {
@@ -51,15 +62,68 @@ export class GroupComponent implements OnInit {
     this.showCreateItemModal = !this.showCreateItemModal;
   }
 
+  public toggleUpdateItemModal() {
+    this.showEditItemModal = !this.showEditItemModal;
+  }
+
+  public closeUpdateItemModal() {
+    this.toggleUpdateItemModal();
+    this.getGroup();
+  }
+
+  public selectItem(itemId: string) {
+    this.selectedItem = this.group.items.find((item) => item._id == itemId) as IItem;
+    this.toggleUpdateItemModal();
+  }
+
   public createItem() {
     this.itemsService
-      .create({ name: this.itemName, description: this.itemDescription, groupId: this.group._id })
+      .create({
+        name: this.itemName,
+        description: this.itemDescription,
+        count: this.itemCount,
+        groupId: this.group._id,
+      })
       .subscribe(() => {
         this.itemName = '';
         this.itemDescription = '';
         this.getGroup();
         this.toggleItemModal();
       });
+  }
+
+  public updateItem() {
+    this.itemsService
+      .update(this.groupId, this.selectedItem._id, {
+        name: this.selectedItem.name,
+        description: this.selectedItem.description,
+        count: this.selectedItem.count,
+      })
+      .subscribe(() => {
+        this.getGroup();
+        this.toggleUpdateItemModal();
+      });
+  }
+
+  public toggleHistoryModal() {
+    this.showHistoryModal = !this.showHistoryModal;
+  }
+
+  public openHistoryModal() {
+    this.getHistory();
+    this.toggleHistoryModal();
+  }
+
+  public listHistory() {
+    this.offsetHistory += 1;
+    this.historyService
+      .get(this.group._id, this.offsetHistory, 30)
+      .subscribe((_) => (this.history = [...this.history, ..._]));
+  }
+
+  public getHistory() {
+    this.offsetHistory = 1;
+    this.historyService.get(this.group._id, this.offsetHistory, 30).subscribe((_) => (this.history = _));
   }
 
   public getGroup() {
