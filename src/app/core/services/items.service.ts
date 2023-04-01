51d89751eb90aeebd51b7ded5d2d7dc7fb5313ae
thisislamer <mangushev.maks13@gmail.com>
2023-04-01 23:26:10 +0300

    add: update item

--- src/app/core/services/items.service.ts
+++ src/app/core/services/items.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environment';
-import { IItem, IItemCreate } from './dto/item.dto';
+import { IItem, IItemCreate, IItemUpdate } from './dto/item.dto';
 
 const baseUrl = `${environment.apiUrl}/items`;
 
@@ -11,5 +11,9 @@ export class ItemsService {
 
   public create(itemCreateDto: IItemCreate) {
     return this.http.post<IItem>(baseUrl, itemCreateDto);
+  }
+
+  public update(groupId: string, itemId: string, updateItemDto: IItemUpdate) {
+    return this.http.put(`${baseUrl}/${groupId}/${itemId}`, updateItemDto);
   }
 }
